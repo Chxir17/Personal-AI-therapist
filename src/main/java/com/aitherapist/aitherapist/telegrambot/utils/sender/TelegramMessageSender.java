a559d973edd2188ec6bc385cbb94b875bd8f1597
Ylcoal <s.samokhin@g.nsu.ru>
2025-07-07 16:48:34 +0700

    add json parser

--- src/main/java/com/aitherapist/aitherapist/telegrambot/utils/sender/TelegramMessageSender.java
+++ src/main/java/com/aitherapist/aitherapist/telegrambot/utils/sender/TelegramMessageSender.java
@@ -3,17 +3,18 @@ package com.aitherapist.aitherapist.telegrambot.utils.sender;
 import com.aitherapist.aitherapist.telegrambot.TelegramBotService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 @RequiredArgsConstructor
 public class TelegramMessageSender implements IMessageSender {
-    private final TelegramBotService botService;
+    private final TelegramLongPollingBot bot;
 
     @Override
     public void sendMessage(long chatId, String text) throws TelegramApiException {
         SendMessage message = new SendMessage(String.valueOf(chatId), text);
-        botService.sendMessage(message);
+        bot.execute(message);
     }
 }
