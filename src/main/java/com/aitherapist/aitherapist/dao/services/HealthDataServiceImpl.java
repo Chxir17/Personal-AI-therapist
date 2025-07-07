d94c6de5c78eb616396f86fe2f84bb427fe77397
Самохин Серафим Вадимович <s.samokhin@g.nsu.ru>
2025-07-07 04:56:17 +0000

    edit commit

--- src/main/java/com/aitherapist/aitherapist/dao/services/HealthDataServiceImpl.java
+++ src/main/java/com/aitherapist/aitherapist/dao/services/HealthDataServiceImpl.java
@@ -39,7 +39,7 @@ public class HealthDataServiceImpl implements IHealthDataService {
     public HealthData updateHealthData(HealthData healthData, Integer userId) {
         HealthData currentHealthData = healthDataRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
-        BeanUtils.copyProperties(healthData, currentHealthData, "id"); // ignore id, чтобы
+        BeanUtils.copyProperties(healthData, currentHealthData, "id"); // ignore id
         return healthDataRepository.save(currentHealthData);
     }
 
