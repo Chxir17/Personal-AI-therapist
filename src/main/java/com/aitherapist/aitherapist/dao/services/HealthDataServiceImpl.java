7e7a2f364880e596ea17a524205b46ddd6cad620
Ylcoal <s.samokhin@g.nsu.ru>
2025-07-07 11:39:07 +0700

    add implementation service with main methods for database work

--- src/main/java/com/aitherapist/aitherapist/dao/services/HealthDataServiceImpl.java
+++ src/main/java/com/aitherapist/aitherapist/dao/services/HealthDataServiceImpl.java
@@ -2,22 +2,51 @@ package com.aitherapist.aitherapist.dao.services;
 
 import com.aitherapist.aitherapist.dao.repositorys.IHealthDataRepository;
 import com.aitherapist.aitherapist.db.entities.HealthData;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
+/**
+ * HealthDataServiceImpl - provide main operation for db
+ * IHealthDataRepository -  repository-interface. auto create interface with implemented main methods.
+ */
 @Service
-public class HealthDataServiceImpl {
-    private final IHealthDataRepository healthDataRepository;
+public class HealthDataServiceImpl implements IHealthDataService {
 
-    public HealthDataServiceImpl(IHealthDataRepository healthDataRepository) {
-        this.healthDataRepository = healthDataRepository;
+    @Autowired
+    private IHealthDataRepository healthDataRepository;
+
+    @Override
+    public HealthData saveHealthData(HealthData healthData) {
+        return healthDataRepository.save(healthData);
     }
 
-    public String getUserHealthStatus(Integer id) {
-        Optional<HealthData> healthData = healthDataRepository.findById(id);
-        return healthData
-                .map(data -> data.toString())
-                .orElse("No data");
+    @Override
+    public List<HealthData> fetchHealhDataList(Integer userId) {
+        return healthDataRepository.findAll();
+    }
+
+    /**
+     * findById, copyProperties - function from spring.
+     * replace multi set.properties.
+     * @param healthData
+     * @param userId
+     * @return
+     */
+    @Override
+    public HealthData updateHealthData(HealthData healthData, Integer userId) {
+        HealthData currentHealthData = healthDataRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        BeanUtils.copyProperties(healthData, currentHealthData, "id"); // ignore id, чтобы
+        return healthDataRepository.save(currentHealthData);
+    }
+
+    @Override
+    public void deleteHealthData(Integer userId) {
+        HealthData healthData =  healthDataRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        healthDataRepository.delete(healthData);
     }
 }
