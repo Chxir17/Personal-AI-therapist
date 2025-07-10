3ef2c946d37e1ac4b6ac370531d1989d16259824
Ylcoal <s.samokhin@g.nsu.ru>
2025-07-10 12:00:37 +0700

    add delete fils and edit startDoctors command

---
+++ src/main/java/com/aitherapist/aitherapist/telegrambot/commands/IDoctorsCommands.java
@@ -0,0 +1,11 @@
+package com.aitherapist.aitherapist.telegrambot.commands;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public interface IDoctorsCommands {
+    boolean verify(Update update) throws TelegramApiException;
+    void apply(Update update);
+    void sendMessageToUser(Long userId, String message) throws TelegramApiException;
+
+}
