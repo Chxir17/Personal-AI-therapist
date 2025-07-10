604dcea819ff93c7a39ad0a039f3aa0b320f8fd8
Ylcoal <s.samokhin@g.nsu.ru>
2025-07-10 10:25:21 +0700

    add UserActivity file and table mysql

---
+++ src/main/java/com/aitherapist/aitherapist/db/entities/UserActivityLog.java
@@ -0,0 +1,33 @@
+package com.aitherapist.aitherapist.db.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * UserActivityLog - for pusher message. Keep time user activity.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "user_activity_logs")
+public class UserActivityLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "action_time", nullable = false)
+    private LocalDateTime actionTime;
+
+    @Column(name = "action_type", nullable = false)
+    private String actionType = "read"; //default ?
+
+}
