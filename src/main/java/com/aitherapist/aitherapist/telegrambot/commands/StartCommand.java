e1841b0ac60bcc6f059d7e2dd2efebc3d57f8ac5
ChxirMac <zyryanov2005@gmail.com>
2025-07-13 15:28:52 +0700

    small fixes up recommendation system

--- src/main/java/com/aitherapist/aitherapist/telegrambot/commands/StartCommand.java
+++ src/main/java/com/aitherapist/aitherapist/telegrambot/commands/StartCommand.java
@@ -1,6 +1,6 @@
 package com.aitherapist.aitherapist.telegrambot.commands;
 
-import com.aitherapist.aitherapist.services.registration.UserRegistrationService;
+import com.aitherapist.aitherapist.services.UserServiceImpl;
 import com.aitherapist.aitherapist.domain.enums.Answers;
 import com.aitherapist.aitherapist.telegrambot.utils.createButtons.InlineKeyboardFactory;
 import com.aitherapist.aitherapist.telegrambot.utils.sender.IMessageSender;
@@ -9,22 +9,20 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import com.aitherapist.aitherapist.telegrambot.messageshandler.contexts.RegistrationContext;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Component
 public class StartCommand implements ICommand {
     private final IMessageSender messageSender;
-    private final UserRegistrationService userRegistrationService;
+    private final UserServiceImpl userRegistrationService;
     private final RegistrationContext registrationContext;
 
     @Autowired
-    public StartCommand(IMessageSender messageSender, UserRegistrationService userRegistrationService, RegistrationContext registrationContext) {
+    public StartCommand(IMessageSender messageSender, UserServiceImpl userRegistrationService, RegistrationContext registrationContext) {
         this.messageSender = messageSender;
         this.userRegistrationService = userRegistrationService;
         this.registrationContext = registrationContext;
@@ -35,7 +33,7 @@ public class StartCommand implements ICommand {
         Long userId = update.getMessage().getFrom().getId();
         long chatId = update.getMessage().getChatId();
 
-        if(!userRegistrationService.isSignUp(Math.toIntExact(userId))) {
+        if(!userRegistrationService.isSignUp(userId)) {
             Map<String, String> buttons = new HashMap<>();
             buttons.put("Доктор", "/doctor");
             buttons.put("Пациент не привязанный к клинике", "/clinicPatient");
