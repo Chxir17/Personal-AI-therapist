a9f4222da500ccc6cf28d14148660df5fe1a41ad
ChxirMac <zyryanov2005@gmail.com>
2025-07-14 12:53:59 +0700

    small fixes up recommendation system

--- src/main/java/com/aitherapist/aitherapist/scheduled/notifications/algorithm/GetUserNotificationTime.java
+++ src/main/java/com/aitherapist/aitherapist/scheduled/notifications/algorithm/GetUserNotificationTime.java
@@ -1,23 +1,31 @@
-//package com.aitherapist.aitherapist.scheduled.notifications.algorithm;
-//
-//import com.aitherapist.aitherapist.services.activity.UserActivityRegistrationService;
-//import com.aitherapist.aitherapist.domain.model.entities.UserActivityLog;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//
-//import java.time.DayOfWeek;
-//import java.time.LocalTime;
-//import java.util.List;
-//import java.util.Map;
-//
-////@Component
-////public class GetUserNotificationTime {
-////    @Autowired
-////    private UserActivityRegistrationService timeLogs;
-////
-////    public Map<DayOfWeek, LocalTime> getTimeForNotificationById(long userID) {
-////        List<UserActivityLog> logs = timeLogs.getByUserId(userID);
-////        Map<DayOfWeek, LocalTime> timeToDayMap = NotificationTimeAnalyzer.getBestNotificationTimesForUser(logs);
-////        return timeToDayMap;
-////    }
-////}
+package com.aitherapist.aitherapist.scheduled.notifications.algorithm;
+
+import com.aitherapist.aitherapist.domain.model.entities.NotificationConfig;
+import com.aitherapist.aitherapist.domain.model.entities.User;
+import com.aitherapist.aitherapist.domain.model.entities.UserActivityLog;
+import com.aitherapist.aitherapist.services.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+
+@Component
+public class GetUserNotificationTime {
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+    public LocalTime getTimeForNotificationById(long userID) {
+        User user = userServiceImpl.getUserByUserId(userID);
+        NotificationConfig notificationConfig = user.getNotificationConfig();
+        boolean isEnabled = notificationConfig.isNotificationsEnabled();
+        LocalTime notificationUserTime = notificationConfig.getNotificationUserTime();
+        if (isEnabled && notificationUserTime != null) {
+            return notificationConfig.getNotificationUserTime();
+        } else if(isEnabled && notificationUserTime == null){
+            return null;
+        }
+        else {
+            return null;
+        }
+    }
+}
